d2788b7ae131b343b235052bfe392a824c42f68b
Manoj2204 <mahtomanojkumar824@gmail.com>
2024-01-17 14:50:28 +0530

    First Commit

---
+++ src/test/java/Practice/Flipkart.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Flipkart
+{
+    @Test
+    public void test()
+    {
+        WebDriver driver=new ChromeDriver();
+    }
+
+}
